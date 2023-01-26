c25c36fa5370d8c192762aefea7f9f205ee36cf2
JacobDangCS50 <jacobohdang@gmail.com>
2023-01-26 22:05:22 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-remake`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
